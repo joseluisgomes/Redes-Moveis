cd9b2c23cc257497c7daab182906adbaa5bb88a8
Rui Cunha <101945306+ruipCunha0@users.noreply.github.com>
2022-10-04 18:29:50 +0100

    Add files via upload

--- CDMA/receiver/rui_version.py
+++ CDMA/receiver/rui_version.py
@@ -1,4 +1,3 @@
-from array import array
 
 
 def convert_to_volts(bits):
@@ -10,21 +9,44 @@ def convert_to_volts(bits):
             volts.append(-1)
     return volts
 
+def sum_signal(signal, factor):
+    
+    index = 0
+    index_2 = 0
+    array = []
+    while index < len(signal):
+        
+        sum = 0
+        index_2 = 0
+        print(index)
+        while index_2 < factor:
+            
+            sum = sum + signal[index]
+            index_2 += 1 
+        
+        if sum > 0:
+            array.append(1)
+        else:
+            array.append(0)
+            
+        index += index_2
+    
+    return array
+            
 
-def multiply(code, signal):
+def multiply(signal, code):
 
     index = 0
     position_spreading_code = 0
     array = []
-    while index <= len(signal):
+    while index < len(signal):
 
         if position_spreading_code == len(code):    # Returns to position 0 when no more chips in code
             position_spreading_code = 0
-
+        
         array.append(int(signal[index]) * int(code[position_spreading_code]))
         position_spreading_code += 1
         index += 1
-
 
     return array
 
@@ -36,12 +58,15 @@ def signal_data(file, signal_id):
             sampled_signal = file_data[0].strip().split(",")  # Sampled CDMA signal
             data_sequence = file_data[1].strip().split(",")  # Transmitted data
             spreading_code = convert_to_volts(file_data[2].strip().split(","))  
-            spreading_factor = file_data[3]
+            spreading_factor = int(file_data[3])
 
-            sampled_signal_multiplied = multiply(spreading_code, sampled_signal)
+            sampled_signal_multiplied = multiply(sampled_signal, spreading_code)
             
             print(sampled_signal_multiplied)
-            print("Done")
+            
+            summed_up_signals = sum_signal(sampled_signal_multiplied, spreading_factor)
+            
+            print(summed_up_signals)
 
             
                
