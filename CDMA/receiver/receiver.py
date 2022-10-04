42a89f55f0d139c53f87aef9cd4209ecd1449cc5
joseluisgomes <joslus.luis@gmail.com>
2022-10-05 00:45:48 +0100

    Divisão da função signal_data() em 2 funções distintas

--- CDMA/receiver/receiver.py
+++ CDMA/receiver/receiver.py
@@ -19,7 +19,7 @@ def multiply(signal, chips):  # Multiply each signal bit with each chip's bit
     return product_result
 
 
-def signal_data(signal_file):
+def signal_process(signal_file):
     try:
         with open(signal_file) as filestream:
             lines = filestream.readlines()
@@ -27,46 +27,62 @@ def signal_data(signal_file):
             transmitted_data = lines[1].strip().split(',')  # 2nd line  (transmitted_data)
             spreading_code = lines[2].strip().split(',')  # 3rd line  (spreading_code)
             spreading_factor = float(lines[3].strip())  # 4th line  (spreading_factor)
+            samples_per_chip = len(spreading_code) / int(spreading_factor)
 
-            total_sum = 0.0
-            sc_mapped = map(float, spreading_code)  # Spreading code mapped into a floats map
-            bits_in_volts = bits_to_volts(list(sc_mapped))
-            multiply_results = multiply(signal, bits_in_volts)
-
-            index = 0
-            while index < len(transmitted_data):
-                sum_results = []
-                f = 0
-                while f < len(multiply_results):
-                    t = 0
-                    while t < spreading_factor:
-                        total_sum += float(multiply_results[int(f + t)])
-                        t += 1
-                    if total_sum > 0:
-                        sum_results.append("1")
-                    else:
-                        sum_results.append("0")
-                    total_sum = 0
-                    f = f + spreading_factor
-
-                errors = 0
-                data_index = 0
-                while data_index < len(transmitted_data):
-                    if sum_results[data_index] != transmitted_data[data_index]:
-                        errors += 1
-                    data_index = data_index + 1
-                index += 1
-        return {"errors": errors, "BER": errors / 1000}  # TODO: MUDAR O VALOR 1000
+            return {"signal": signal, "transmitted_data": transmitted_data,
+                    "spreading_code": spreading_code, "spreading_factor": spreading_factor,
+                    "samples_per_chip": samples_per_chip}
     except OSError:
         print(f"Failed to open {signal_file} file")
         exit(0)
 
 
+def signal_errors(signal_data):  # Calculate the BER of the given signal
+    signal = signal_data["signal"]
+    transmitted_data = signal_data["transmitted_data"]
+    spreading_code = signal_data["spreading_code"]
+    spreading_factor = signal_data["spreading_factor"]
+
+    total_sum = 0.0
+    sc_mapped = map(float, spreading_code)  # Spreading code mapped into a floats map
+    bits_in_volts = bits_to_volts(list(sc_mapped))
+    multiply_results = multiply(signal, bits_in_volts)
+
+    index = 0
+    errors = 0
+    while index < len(transmitted_data):
+        sum_results = []
+        f = 0
+        while f < len(multiply_results):
+            t = 0
+            while t < spreading_factor:
+                total_sum += float(multiply_results[int(f + t)])
+                t += 1
+            if total_sum > 0:
+                sum_results.append("1")
+            else:
+                sum_results.append("0")
+            total_sum = 0
+            f = f + spreading_factor
+
+        errors = 0
+        data_index = 0
+        while data_index < len(transmitted_data):
+            if sum_results[data_index] != transmitted_data[data_index]:
+                errors += 1
+            data_index = data_index + 1
+        index += 1
+    return {"errors": errors, "BER": errors / 1000}  # TODO: MUDAR O VALOR 1000
+
+
 signal1_file = "e1.txt"
 signal2_file = "e2.txt"
 
+signal1_data = signal_process(signal1_file)
+signal2_data = signal_process(signal2_file)
+
 print(f"---- Signal {1} ----")
-print(signal_data(signal1_file))
+print(signal_errors(signal1_data))
 
 print(f"\n---- Signal {2} ----")
-print(signal_data(signal2_file))
+print(signal_errors(signal2_data))
