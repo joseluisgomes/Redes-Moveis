0629babd0a4c07ae3f44340e24db6d670d1a6a3a
Rui Cunha <101945306+ruipCunha0@users.noreply.github.com>
2022-10-04 18:43:46 +0100

    Add files via upload

--- CDMA/receiver/rui_version.py
+++ CDMA/receiver/rui_version.py
@@ -4,36 +4,49 @@ def convert_to_volts(bits):
     volts = list()
     for bit in bits:
         if bit == "0":
-            volts.append(1)
+            volts.append(1) # if volt = '0' turn into 1
         else:
-            volts.append(-1)
+            volts.append(-1) # if volt = '-1' turn into -1
     return volts
 
 def sum_signal(signal, factor):
     
-    index = 0
+    index = 0   
     index_2 = 0
     array = []
-    while index < len(signal):
+    while index < len(signal):  # incrementar ate ao ultimo sampled_signal
         
         sum = 0
         index_2 = 0
-        print(index)
-        while index_2 < factor:
+        while index_2 < factor: # incrementar ate 5 (spreading_factor)
             
-            sum = sum + signal[index]
+            sum = sum + signal[index]   # somar os valores 5 vezes
             index_2 += 1 
         
         if sum > 0:
-            array.append(1)
+            array.append(1) # se valor > 0 guardar 1
         else:
-            array.append(0)
+            array.append(0) # se valor < 0 guardar 0
             
         index += index_2
     
     return array
             
-
+def error_detector(signal, trasmitted_data):
+    
+    index = 0
+    erros = 0
+    while index < len(trasmitted_data): # incrementar ate igualar o data_sequence
+        
+        if int(signal[index]) != int(trasmitted_data[index]):   # se valor do signal != data_sequence, erro detado
+            erros += 1
+            
+        index += 1
+        
+    return erros
+        
+    
+    
 def multiply(signal, code):
 
     index = 0
@@ -60,13 +73,21 @@ def signal_data(file, signal_id):
             spreading_code = convert_to_volts(file_data[2].strip().split(","))  
             spreading_factor = int(file_data[3])
 
-            sampled_signal_multiplied = multiply(sampled_signal, spreading_code)
+            sampled_signal_multiplied = multiply(sampled_signal, spreading_code)    # executar multiplicaçao entre o sampled_signal e o sreading_code
             
             print(sampled_signal_multiplied)
             
-            summed_up_signals = sum_signal(sampled_signal_multiplied, spreading_factor)
+            summed_up_signals = sum_signal(sampled_signal_multiplied, spreading_factor) # executar a soma dos sinais multiplicados de cinco em cinco
             
             print(summed_up_signals)
+            
+            errors = error_detector(summed_up_signals, data_sequence)   # comparar os valores calculados com o data_sequence para descobrir o nº de erros
+            
+            print(f"---- Signal {signal_id} ----")
+            print("Errors: " + str(errors))
+            print("BER: " + str(errors / 1000))
+            
+            
 
             
                
