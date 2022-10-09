320dbb72e976f92ce0fab873e28314c506142f42
joseluisgomes <joslus.luis@gmail.com>
2022-10-09 14:29:09 +0100

    Repartição dos ficheiros dos sinais consoante a fase respetiva

--- untitled/src/ReceiverTestDrive.java
+++ untitled/src/ReceiverTestDrive.java
@@ -1,5 +1,5 @@
 public class ReceiverTestDrive {
-    private static final String filePath = "/home/joseluisgomes/Github/UMinho/Redes-Moveis/untitled/signals/"; // TODO: Change the file path
+    private static final String filePath = "/home/joseluisgomes/Github/UMinho/Redes-Moveis/untitled/signals/fase_1/"; // TODO: Change the file path
 
     public static void main(String[] args) {
         final var receiver = new Receiver(filePath + "c3_-0.5dB.txt");
